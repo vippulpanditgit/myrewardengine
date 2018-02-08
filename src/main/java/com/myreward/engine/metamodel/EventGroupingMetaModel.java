63672cc4d069848f7ac7f218c1f260c0702a0a6e
vippulpanditgit@gmail.com <vippul.pandit.git@gmail.com>
2018-02-07 20:15:58 -0800

    Removed EventGroupingMEtaModel

--- src/main/java/com/myreward/engine/metamodel/EventGroupingMetaModel.java
+++
@@ -1,9 +0,0 @@
-package com.myreward.engine.metamodel;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class EventGroupingMetaModel extends BaseMetaModel {
-	public List<GroupMetaModel> groupMetaModelList = new ArrayList<GroupMetaModel>();
-
-}
