254e14ce73e8ef32582cd29b798bff1947be5bb7
vippulpanditgit@gmail.com <vippul.pandit.git@gmail.com>
2018-02-12 20:00:27 -0800

    Show setup

--- src/main/java/com/myreward/parser/metamodel/RewardMetaModel.java
+++ src/main/java/com/myreward/parser/metamodel/RewardMetaModel.java
@@ -1,19 +1,35 @@
 package com.myreward.parser.metamodel;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import com.myreward.parser.grammar.MyRewardParser;
+import com.myreward.parser.symbol.Symbol;
+import com.myreward.parser.symbol.SymbolTable;
 
 public class RewardMetaModel extends BaseMetaModel {
 	public enum RewardType {
 		DOLLAR,
 		POINTS
 	}
-	private String[] setRewardOpCodeListTemplate = {"lbl_rwd:%d", "store_rwd_flg(%d)", "return"};
+	private String[] rewardOpCodeListTemplate = {"lbl_rwd:%d", "store_rwd_flg(%d)", "store_rwd_amt(%d, %f)", "return"};
 	public RewardType rewardType;
 	public double rewardAmount;
 	@Override
 	public String[] build() {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> rewardOpCodeList = new ArrayList<String>();
+		if(this.parent instanceof EventMetaModel) {
+			EventMetaModel parentEventMetaModel = (EventMetaModel)this.parent;
+			Symbol eventSymbol = new Symbol(parentEventMetaModel.getEventName());
+			SymbolTable symbolTable = MyRewardParser.symbolTable;
+			eventSymbol = symbolTable.lookup(eventSymbol);
+			rewardOpCodeList.add(String.format(rewardOpCodeListTemplate[0], eventSymbol.getFullyQualifiedId()));
+			rewardOpCodeList.add(String.format(rewardOpCodeListTemplate[1], eventSymbol.getFullyQualifiedId()));
+			rewardOpCodeList.add(String.format(rewardOpCodeListTemplate[2], eventSymbol.getFullyQualifiedId(), rewardAmount));
+			rewardOpCodeList.add(String.format(rewardOpCodeListTemplate[3], eventSymbol.getFullyQualifiedId()));
+		}
+		return rewardOpCodeList.toArray(new String[0]);
 	}
 	@Override
 	public String[] model() {
--- src/main/java/com/myreward/parser/metamodel/ShowMetaModel.java
+++ src/main/java/com/myreward/parser/metamodel/ShowMetaModel.java
@@ -1,5 +1,12 @@
 package com.myreward.parser.metamodel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.myreward.parser.grammar.MyRewardParser;
+import com.myreward.parser.symbol.Symbol;
+import com.myreward.parser.symbol.SymbolTable;
+
 public class ShowMetaModel extends BaseMetaModel {
 	public boolean isShow;
 
@@ -7,8 +14,17 @@ public class ShowMetaModel extends BaseMetaModel {
 
 	@Override
 	public String[] build() {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> showOpCodeList = new ArrayList<String>();
+		if(this.parent instanceof EventMetaModel) {
+			EventMetaModel parentEventMetaModel = (EventMetaModel)this.parent;
+			Symbol eventSymbol = new Symbol(parentEventMetaModel.getEventName());
+			SymbolTable symbolTable = MyRewardParser.symbolTable;
+			eventSymbol = symbolTable.lookup(eventSymbol);
+			showOpCodeList.add(String.format(showOpCodeListTemplate[0], eventSymbol.getFullyQualifiedId()));
+			showOpCodeList.add(String.format(showOpCodeListTemplate[1], eventSymbol.getFullyQualifiedId()));
+			showOpCodeList.add(String.format(showOpCodeListTemplate[2], eventSymbol.getFullyQualifiedId()));
+		}
+		return showOpCodeList.toArray(new String[0]);
 	}
 
 	@Override
