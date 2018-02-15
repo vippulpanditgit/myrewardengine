167ea34fdba666a7bc625896db4f54ff8a78e5ea
vippulpanditgit@gmail.com <vippul.pandit.git@gmail.com>
2018-02-14 20:16:49 -0800

    experimentation on test.java

--- src/main/java/com/myreward/parser/test/test.java
+++ src/main/java/com/myreward/parser/test/test.java
@@ -69,7 +69,7 @@ public class test {
 			String pseudo_gatekeeper = "package test event(GA).any(1) {event(B), event(C)}.show(true).priority(1) event(GD).any(1){event(E), event(F)}.gatekeeper(event(GA))";
 			String pseudo_standalone_gatekeeper = "package test event(A).any(1).show(true).priority(1) event(B).gatekeeper(event(A)).reward(100)";			
 			
-			MyRewardParser myRewardParser = MyRewardParserUtil.getParsed(pseudo);
+			MyRewardParser myRewardParser = MyRewardParserUtil.getParsed(pseudo_2_groups);
 	        
             Myreward_defsContext fileContext = myRewardParser.myreward_defs(); 
             
