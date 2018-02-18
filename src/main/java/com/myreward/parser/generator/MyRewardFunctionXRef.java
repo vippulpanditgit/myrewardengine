73d495c6f4773b1211dd47e020666e50b25fe57d
vippulpanditgit <vippul@cox.net>
2018-02-18 05:27:02 -0800

    Fix for 'model' for standalone events

---
+++ src/main/java/com/myreward/parser/generator/MyRewardFunctionXRef.java
@@ -0,0 +1,9 @@
+package com.myreward.parser.generator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyRewardFunctionXRef {
+	public static Map<String, String> fnXRef = new HashMap<String, String>();
+
+}
