e6ed2265561506b69fe8a1b30daccc5b2a624639
vippulpanditgit <vippul@cox.net>
2018-02-22 04:37:31 -0800

    Updating Model for running engine

--- src/main/java/com/myreward/engine/event/opcode/CallBaseModel.java
+++ src/main/java/com/myreward/engine/event/opcode/CallBaseModel.java
@@ -1,5 +1,5 @@
 package com.myreward.engine.event.opcode;
 
 public class CallBaseModel extends OpCodeBaseModel {
-
+	protected static String ARGUMENT_SEPERATOR = ":";
 }
--- src/main/java/com/myreward/engine/event/opcode/CallShowModel.java
+++ src/main/java/com/myreward/engine/event/opcode/CallShowModel.java
@@ -1,5 +1,20 @@
 package com.myreward.engine.event.opcode;
 
-public class CallShowModel extends CallBaseModel {
+import org.apache.commons.lang3.StringUtils;
 
+public class CallShowModel extends CallBaseModel {
+	private static String OPCODE_LABEL = "call_shw";
+	private static String OPERAND_FORMAT = "(%s:%s)";
+	private String name;
+	private String version;
+	
+	public CallShowModel(String statement) {
+		String[] fields = StringUtils.split(statement, ARGUMENT_SEPERATOR);
+		if(fields.length==3) {
+			if(!StringUtils.equalsIgnoreCase(fields[0], OPCODE_LABEL))
+				return;
+			name = fields[1];
+			version = fields[2];
+		}
+	}
 }
