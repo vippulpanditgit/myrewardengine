25266930090d50568d9d73beb486ea41b7d4df82
vippulpanditgit <vippul@cox.net>
2018-04-14 14:54:55 -0700

    import functionality start

--- src/main/java/com/myreward/parser/util/FileProcessingUtil.java
+++ src/main/java/com/myreward/parser/util/FileProcessingUtil.java
@@ -4,12 +4,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FileProcessingUtil {
-	public static String readFile(String fileName) {
+	public static String readFile(String fileName) throws IOException {
 		List<String> list = new ArrayList<>();
 
 		try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
@@ -18,8 +19,11 @@ public class FileProcessingUtil {
 
 		} catch (IOException e) {
 			e.printStackTrace();
+			throw e;
 		}
 		return list.stream().map(e -> e.toString()).reduce("", String::concat);
 	}
-
+	public static void writeFile(String fileName, String[] data) throws IOException {
+		Files.write(Paths.get(fileName), (Iterable<String>)Arrays.stream(data)::iterator);
+	}
 }
