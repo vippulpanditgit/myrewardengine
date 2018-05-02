9a752b37bdc4d30e730e9bd388085b755da42bf6
vippulpanditgit@gmail.com <vippul.pandit.git@gmail.com>
2018-05-01 19:57:39 -0700

    Reward change based on event cound

---
+++ src/main/java/com/myreward/engine/app/User.java
@@ -0,0 +1,17 @@
+package com.myreward.engine.app;
+
+import java.util.Date;
+import java.util.Map;
+
+public class User {
+	private String firstName;
+	private String lastName;
+	private Date dob;
+	private String address1;
+	private String address2;
+	private String city;
+	private String state;
+	private String zipcode;
+	private Map<String, String> attribute;
+
+}
