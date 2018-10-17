e9bc1e215fc270aea23a8164890c56c8980a5e8c
vippulpanditgit <vippul@cox.net>
2018-10-17 04:46:35 -0700

    vulernability risk fix.

--- src/main/java/com/myreward/parser/test/DynamoDBTest.java
+++ src/main/java/com/myreward/parser/test/DynamoDBTest.java
@@ -23,31 +23,7 @@ public class DynamoDBTest {
 		dynamoDbUtil.createLocal();
 		dynamoDbUtil.putItem("com.test_1.0", null);
 		dynamoDbUtil.getItem("com.test_1.0");
-/*		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(
-				new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-2"))
-				.build(); 
-            DynamoDB dynamoDB = new DynamoDB(client);
 
-            String tableName = "Movies";
-
-            try {
-                System.out.println("Attempting to create table; please wait...");
-                Table table = dynamoDB.createTable(tableName,
-                    Arrays.asList(new KeySchemaElement("year", KeyType.HASH), // Partition
-                                                                              // key
-                        new KeySchemaElement("title", KeyType.RANGE)), // Sort key
-                    Arrays.asList(new AttributeDefinition("year", ScalarAttributeType.N),
-                        new AttributeDefinition("title", ScalarAttributeType.S)),
-                    new ProvisionedThroughput(10L, 10L));
-                table.waitForActive();
-                System.out.println("Success.  Table status: " + table.getDescription().getTableStatus());
-
-            }
-            catch (Exception e) {
-                System.err.println("Unable to create table: ");
-                System.err.println(e.getMessage());
-            }
-*/
 	}
 
 }
